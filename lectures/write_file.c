0099499004b63eab75636af6816b55af3b36068d
Dgabri <gabriele.deri@gmail.com>
2022-10-20 09:04:16 +0200

    updated gitignore and write file

--- lectures/write_file.c
+++ lectures/write_file.c
@@ -7,21 +7,16 @@ int main(int argc, char *argv[]) {
     if (argc != 3) {
         return 1;
     }
+    char *fname;
+    int file_number;
 
-    char *fname = argv[1];
-    FILE *f = fopen(fname, "wt");
-
-    if (f == NULL) {
-        return -1;
+    for (int i = 0; i < file_number; i++) {
+      char *s = NULL;
+      int e = asprintf(&s, "%s.%d", fname, i);
+      if (e < 0)
+        exit(1);
+      // FILE * =
     }
 
-    for (int i = 2; i <= n; i++) {
-        if (isprime(i)) {
-            int e = fprintf(f, "%d\n", i);
-            if (e < 0) return -1
-        }
-    }
-    if (fclose(f) == EOF) { //file close succesfully
-        return -1;
-    }
+    return -1;
 }
